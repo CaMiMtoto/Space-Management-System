82ad2c6efc6bc66c14d9b64f73be951bddaa6452
CaMi <jeanpaulcami@live.com>
2025-01-02 14:22:43 +0200

    add favicon

--- resources/views/components/fav-icon.blade.php
+++ resources/views/components/fav-icon.blade.php
@@ -1,9 +1,7 @@
-{{--
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}" {{ $attributes }}>
-<link rel="icon" type="image/png" sizes="32x32" href="{{ asset("favicon-32x32.png") }}">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ asset("favicon-16x16.png") }}">
-<link rel="manifest" href="{{ asset("site.webmanifest") }}">
-<link rel="mask-icon" href="{{ asset("safari-pinned-tab.svg") }}" color="#5bbad5">
-<meta name="msapplication-TileColor" content="#da532c">
-<meta name="theme-color" content="#ffffff">
---}}
+
+<link rel="icon" type="image/png" href={{ asset('favicon-96x96.png') }}" sizes=" 96x96" />
+<link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}"/>
+<link rel="shortcut icon" href="{{ asset('favicon.ico')  }}"/>
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}"/>
+<meta name="apple-mobile-web-app-title" content="RICA"/>
+<link rel="manifest" href="{{ asset('site.webmanifest') }}" />
